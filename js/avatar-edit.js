f2e77cc6ef251435089b158cad5b8c3f7aa7c4f2
Konstantin Belousob <belolnik21@gmail.com>
2022-12-22 03:13:59 +0500

    Окночание фазы разработки

---
+++ js/avatar-edit.js
@@ -0,0 +1,163 @@
+function editAvatar() {
+
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
+  let image = document.getElementById('editImage');
+
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  ctx.width = image.width;
+  ctx.height = image.height;
+
+  console.log(image);
+
+  // canvas.style.border = '5px solid red';
+
+
+  let canvas_width = canvas.width;
+  let canvas_height = canvas.height;
+  let offset_x, offset_y;
+
+  let get_offset = function () {
+    let canvas_offsets = canvas.getBoundingClientRect();
+    offset_x = canvas_offsets.left;
+    offset_y = canvas_offsets.top;
+  }
+
+  get_offset();
+  window.onscroll = function () { get_offset(); }
+  window.onresize = function () { get_offset(); }
+  canvas.onresize = function () { get_offset(); }
+
+
+  let startX, startY;
+  let is_dragging = false;
+
+
+  let procent_window = 0.96;
+  let shape_width = parseInt(image.width * procent_window);
+
+  let shape = { x: 20, y: 40, width: shape_width, height: shape_width, color: 'red' };
+
+  let is_mouse_in_shape = function (x, y, shape) {
+    let shape_left = shape.x;
+    let shape_right = shape.x + shape.width;
+    let shape_top = shape.y;
+    let shape_bottom = shape.y + shape.height
+
+    if (x > shape_left && x < shape_right && y > shape_top && y < shape_bottom)
+      return true;
+  }
+
+  let mouse_down = function (event) {
+    event.preventDefault();
+
+    startX = parseInt(event.clientX - offset_x);
+    startY = parseInt(event.clientY - offset_y);
+
+    if (is_mouse_in_shape(startX, startY, shape)) {
+      is_dragging = true;
+      return;
+    }
+  }
+
+  let mouse_up = function (event) {
+    if (!is_dragging)
+      return
+
+    event.preventDefault();
+    is_dragging = false;
+  }
+
+  let mouse_out = function (event) {
+    if (!is_dragging)
+      return
+
+    event.preventDefault();
+    is_dragging = false;
+  }
+
+  let mouse_move = function () {
+    if (!is_dragging) {
+      return;
+    } else {
+      event.preventDefault();
+      let mouseX = parseInt(event.clientX - offset_x);
+      let mouseY = parseInt(event.clientY - offset_y);
+
+      let dx = mouseX - startX;
+      let dy = mouseY - startY;
+
+      let current_shape = shape;
+      current_shape.x += dx;
+      current_shape.y += dy;
+
+      draw_shapes();
+
+      startX = mouseX;
+      startY = mouseY;
+    }
+  }
+
+  canvas.onmousedown = mouse_down;
+  canvas.onmouseup = mouse_up;
+  canvas.onmouseout = mouse_out;
+  canvas.onmousemove = mouse_move;
+  
+
+  let touch_start = function (event) {
+    event.preventDefault();
+
+    startX = parseInt(event.changedTouches[0].clientX - offset_x);
+    startY = parseInt(event.changedTouches[0].clientY - offset_y);
+
+    if (is_mouse_in_shape(startX, startY, shape)) {
+      is_dragging = true;
+      return;
+    }
+  }
+  
+  let touch_end = function (event) {
+    if (!is_dragging)
+    return
+    
+    event.preventDefault();
+    is_dragging = false;
+  }
+  
+  let touch_move = function (event) {
+    if (!is_dragging) {
+      return;
+    } else {
+      event.preventDefault();
+      let mouseX = parseInt(event.changedTouches[0].clientX - offset_x);
+      let mouseY = parseInt(event.changedTouches[0].clientY - offset_y);
+
+      let dx = mouseX - startX;
+      let dy = mouseY - startY;
+
+      let current_shape = shape;
+      current_shape.x += dx;
+      current_shape.y += dy;
+
+      draw_shapes();
+
+      startX = mouseX;
+      startY = mouseY;
+    }
+  }
+  
+  canvas.ontouchstart = touch_start;
+  canvas.ontouchend = touch_end;
+  canvas.ontouchmove = touch_move;
+
+
+  let draw_shapes = function () {
+    ctx.clearRect(0, 0, canvas_width, canvas_height);
+
+    ctx.drawImage(image, shape.x, shape.y, 400, 400, shape.x, shape.y, shape.width, shape.height);
+  }
+
+  draw_shapes();
+}
---
+++ js/index.js
@@ -0,0 +1,226 @@
+// Показ/Скрытие пароля
+function show_hide_password(target) {
+  var input = target.previousElementSibling;
+  if (input.getAttribute("type") == "password") {
+    target.classList.add("view");
+    input.setAttribute("type", "text");
+  } else {
+    target.classList.remove("view");
+    input.setAttribute("type", "password");
+  }
+  return false;
+}
+
+// Переключение страниц
+// header = {"frameName": Node, "show": boolean}
+function swapPage(from, to, header) {
+  document.getElementById(from).hidden = true;
+  document.getElementById(to).hidden = false;
+  
+  if (Object.entries(header).length != 0) {
+    header.frameName.getElementsByTagName('header')[0].hidden = !header.show;
+  }
+}
+
+// Отображение услуг в выбранной категории
+function sliderPage(all, to) {
+  pages = document.getElementsByClassName(all);
+  for (let page in pages){
+    pages[page].hidden = true;
+  }
+  document.getElementById(to).hidden = false;
+}
+
+// Раскрытие описание услуги
+function seeService(when, inside=true) {
+  let menu = document.querySelector("header >.menu");
+  when = when.getAttribute("service");
+  if(inside){
+    let back = document.createElement("a");
+    back.href = "#";
+    back.classList.add('back');
+    back.setAttribute("service", when);
+    back.onclick = function () {
+      back.remove();
+      seeService(this, false); 
+    }
+    menu.prepend(back);
+    menu.getElementsByClassName("title")[0].hidden = true;
+    document.getElementById("services").hidden = true;
+    document.getElementById("descriptions").hidden = false;
+    document.getElementById(when).hidden = false;
+  } else{
+    menu.getElementsByClassName("title")[0].hidden = false;
+    document.getElementById(when).hidden = true;
+    document.getElementById("descriptions").hidden = true;
+    document.getElementById("services").hidden = false;
+  }
+}
+
+// Переход со скрытием всех открытых
+function footerSwap(to) {
+  document.querySelector('main>header').hidden = false;
+  document.querySelector('header .notifications').hidden = false;
+  if (document.querySelector('header .settings'))
+    document.querySelector('header .settings').remove();
+
+  for (let selection of document.getElementsByTagName('section'))
+    selection.hidden = true;
+
+  clearService();
+
+  document.getElementById(to).hidden = false;
+}
+
+// Переход на профиль
+function toProfile() {
+  document.querySelector('main>header').hidden = false;
+
+  for (let selection of document.getElementsByTagName("section"))
+    selection.hidden = true;
+  document.querySelector('#profile').hidden = false;
+
+  let menu = document.querySelector('header>.menu');
+  menu.getElementsByClassName('notifications')[0].hidden = true;
+
+  if (menu.querySelectorAll('.settings').length < 1){
+    let settings = document.createElement('a');
+    settings.classList.add('settings');
+    settings.href = '#';
+    settings.onclick = function () {
+      settings.remove();
+      swapPage('profile','settings', {});
+    }
+    menu.append(settings);
+  }
+}
+
+function toNotifications(){
+  for (let selection of document.getElementsByTagName('section'))
+    selection.hidden = true;
+
+  clearService();
+  document.querySelector('header .notifications').hidden = true;
+  document.getElementById('notifications').hidden = false;
+}
+
+
+// Уберает все открытые страницы с описанием услуг
+function clearService() {
+  for (let service of document.getElementsByClassName("service")) {
+    service.hidden = true;
+
+    // Убераем кнопку назад со страниц описания при переходе с кнопки домой
+    let menu = document.querySelector("header >.menu");
+    menu.getElementsByClassName("title")[0].hidden = false;
+    if (menu.getElementsByClassName("back").length != 0) {
+      menu.getElementsByClassName("back")[0].remove();
+    }
+  }
+}
+
+// Развернуть карту
+function expandMap() {
+  let target = event.target;
+  if (target.closest('div.map-big')){
+    target.closest('div.map').classList.remove("map-big");
+    target.src = '/image/map.png';
+    document.querySelector('#contact>.addresses').hidden = false;
+    document.querySelector('#contact>.social').hidden = false;
+    document.querySelector('#contact>.email').hidden = false;
+  } else {
+    target.closest('div.map').classList.add("map-big");
+    target.src = '/image/map-big.png';
+    document.querySelector('#contact>.addresses').hidden = true;
+    document.querySelector('#contact>.social').hidden = true;
+    document.querySelector('#contact>.email').hidden = true;
+  }
+}
+
+// Редактирование профеля
+function editProfile() {
+  let account = document.querySelector('#profile>#account>.account');
+  let garage = document.querySelector('#profile>#account>.garage');
+  let btn_edit = account.querySelector('a#editProfile');
+
+
+  if (btn_edit.innerHTML == "Готово"){
+    // Выход из меню редактирования профиля
+    btn_edit.innerHTML = "Ред.";
+    account.querySelector('img').classList.remove('account-edit');
+    account.querySelector('.account-edit-icon').remove();
+    document.querySelector('#profile>#account>.history').hidden = false;
+    account.querySelector('input.name').readOnly = true;
+    account.querySelector('input.phone').readOnly = true;
+    account.querySelector('input.name+div').remove();
+    account.querySelector('input.phone+div').remove();
+    garage.querySelector('#garage-new').remove();
+    garage.querySelector('#create_garage').hidden = true;
+    return;
+  }
+  // Формирование меню редактирования профиля
+  
+  account.querySelector('img').classList.add('account-edit');
+  // Иконка редактирования изображения
+  let icon = document.createElement('a');
+  icon.href = '#';
+  icon.classList.add('account-edit-icon');
+  icon.onclick = function () {
+    swapPage('profile', 'editAvatar', { "frameName": main, "show": false });
+    editAvatar();
+  }
+  account.append(icon);
+
+  // Редактирование имени и номера телефона
+  let editName = document.createElement('div');
+  editName.href = "#";
+  editName.classList.add('edit');
+  account.querySelector('input.name').readOnly = 
+    !account.querySelector('input.name').readOnly;
+  account.querySelector('input.name').after(editName);
+
+  let editPhone = document.createElement('div');
+  editPhone.href = "#";
+  editPhone.classList.add('edit');
+  account.querySelector('input.phone').readOnly =
+    !account.querySelector('input.phone').readOnly;
+  account.querySelector('input.phone').after(editPhone);
+
+  btn_edit.innerHTML = "Готово";
+  document.querySelector('#profile>#account>.history').hidden = true;
+
+  // Изменение гаража
+  let del_btn = document.createElement('a');  // Кнопка удаления элемента
+  del_btn.href = "#";
+  del_btn.innerHTML = "Удалить";
+  del_btn.classList.add('btn');
+  del_btn.classList.add('enable');
+  del_btn.classList.add('del_auto');
+
+
+  // Создание нового транспорта
+  let new_auto_create = document.createElement('a');
+  new_auto_create.href = '#';
+  new_auto_create.innerHTML = '+';
+  new_auto_create.classList = 'btn';
+  
+  let new_auto = document.createElement('div');
+  new_auto.classList.add('auto');
+  new_auto.id = 'garage-new';
+  new_auto.appendChild(new_auto_create);
+  new_auto.onclick = function () {
+    garage.querySelector('#create_garage').hidden = false;
+    garage.querySelector('#garage-new').hidden = true;
+  }
+
+  garage.querySelectorAll('div.auto').forEach(element => {
+    element.querySelector('.btn').after(del_btn.cloneNode(true));
+  }); 
+  garage.appendChild(new_auto);
+
+  garage.querySelectorAll('div.auto>.del_auto').forEach(element => {
+    element.addEventListener('click', function () {
+      event.target.parentNode.remove();
+    })
+  });
+}
